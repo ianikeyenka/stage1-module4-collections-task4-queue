d17bb01c9ef9578e7ae27072f5a64d10a329eb81
Ilya Anikeyenka <20prorok02@gmail.com>
2023-06-09 21:06:03 +0300

    Solution

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -6,5 +6,19 @@ import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+        ArrayDeque<Integer> result = new ArrayDeque<>();
+        result.add(firstQueue.remove());
+        result.add(firstQueue.remove());
+        result.add(secondQueue.remove());
+        result.add(secondQueue.remove());
+        while (firstQueue.size() != 0) {
+            firstQueue.add(result.removeLast());
+            result.add(firstQueue.remove());
+            result.add(firstQueue.remove());
+            secondQueue.add(result.removeLast());
+            result.add(secondQueue.remove());
+            result.add(secondQueue.remove());
+        }
+        return result;
     }
 }
--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,22 @@
 package com.epam.collections.queue;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        List<Integer> deque = new LinkedList<>();
+        List<Integer> list = new ArrayList<>();
+        for (int i = 1; i <= numberOfDishes; i++) {
+            deque.add(i);
+        }
+        int k = 0;
+        while (!deque.isEmpty()) {
+            k = (k + everyDishNumberToEat - 1) % deque.size();
+            int n = deque.remove(k);
+            list.add(n);
+        }
+        return list;
     }
 }
